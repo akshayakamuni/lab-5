41f91391f38f6691947902224984556197faaade
akshayakamuni <kamuniakshaya22@gmail.com>
2024-09-19 17:14:26 +0530

    Add files via upload

---
+++ q67.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int main()
+{
+	int a[3][3],b[3][3],sub[3][3],i,j;
+	printf("enter the elements of a matrix");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf(" values of elements of a matrix\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("enter the elements of b matrix");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&b[i][j]);
+		}
+	}
+	printf("values of the elements of b matrix \n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d\t",b[i][j]);
+		}
+		printf("\n");
+	}
+	printf("sub of a and b matrices\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			sub[i][j]=a[i][j]-b[i][j];
+			printf("%d\t",sub[i][j]);
+		}
+		printf("\n");
+	}
+}
