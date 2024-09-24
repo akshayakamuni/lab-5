01f7ffb82d41f9536da16dad01fb57b1b750ab04
akshayakamuni <kamuniakshaya22@gmail.com>
2024-09-24 20:18:41 +0530

    Add files via upload

---
+++ q50.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int i,j,rows,a;
+	rows=5;
+	for(i=0;i<=rows;i++){
+	 int a=65;  
+	for(j=0;j<=i;j++){
+	printf("%c",a+i);
+	
+	}
+	printf("\n");
+
+	}
+	return 0;
+}
